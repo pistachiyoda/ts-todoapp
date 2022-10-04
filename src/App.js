2e642302eecb12af949eee0cf4326127636f8230
mai <fujisawamai21@gmail.com>
2022-10-04 23:13:11 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { TodoListModel } from "./model/TodoListModel.js";
+import { TodoItemModel } from "./model/TodoItemModel.js";
+import { TodoListView } from "./view/TodoListView.js";
+import { render } from "./view/html-util.js";
+
+export class App {
+    #todoListView = new TodoListView();
+    #todoListModel = new TodoListModel([]);
+
+    handleAdd(title) {
+        this.#todoListModel.addTodo(new TodoItemModel({ title, completed: false}))
+    }
+
+    handleUpdate({ id, completed }) {
+        this.#todoListModel.updateTodo({ id, completed });
+    }
+
+    handleDelete({ id }) {
+        this.#todoListModel.deleteTodo({ id });
+    }
+    
+    mount() {
+        const formElement = document.querySelector('#js-form');
+        const inputElement = document.querySelector('#js-form-input');
+        const containerElement = document.querySelector("#js-todo-list");
+        const todoItemCountElement = document.querySelector("#js-todo-count");
+        
+        this.#todoListModel.onChange(() => {
+            const todoItems = this.#todoListModel.getTodoItems();
+            const todoListElement = this.#todoListView.createElement(todoItems, {
+                onUpdateTodo: ({ id, completed }) => {
+                    this.handleUpdate({ id, completed });
+                },
+                onDeleteTodo: ({ id }) => {
+                    this.handleDelete({ id });
+                }
+            });
+            render(todoListElement, containerElement);
+            todoItemCountElement.textContent = `Todoアイテム数: ${this.#todoListModel.getTotalCount()}`;
+        });
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+            this.handleAdd(inputElement.value);
+            inputElement.value = "";
+        });
+    }
+}
---
+++ src/view/html-util.js
@@ -0,0 +1,31 @@
+export function escapeSpecialChars(str) {
+    return str
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+export function htmlToElemnt(html) {
+    const template = document.createElement("template");
+    template.innerHTML = html;
+    return template.content.firstElementChild;
+}
+
+export function element(strings, ...values) {
+    const htmlString = strings.reduce((result, str, i) => {
+        const value = values[i - 1];
+        if (typeof value === "string") {
+            return result + escapeSpecialChars(value) + str;
+        } else {
+            return result + String(value) + str;
+        }
+    });
+    return htmlToElemnt(htmlString);
+}
+
+export function render(bodyElement, containerElement) {
+    containerElement.innerHTML = "";
+    containerElement.appendChild(bodyElement);
+}
